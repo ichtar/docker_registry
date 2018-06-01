b46fda215f8915e4f4ab941ed240cd0076612bb0
Jean-Noël Vouilloz <jean-noel.vouilloz@bestmile.com>
2018-06-01 14:14:29 +0200

    read image and respective tags

--- registry.go
+++ registry.go
@@ -2,16 +2,14 @@ package main
 
 import (
 	"fmt"
+	"strings"
         "net/http"
 	"io/ioutil"
 	"encoding/json"
 )
 
-func main() {
-    type Message struct {
-        Repositories []string `json:"repositories"`
-    }
-    resp, err := http.Get("http://localhost:5000/v2/_catalog")
+func read_http_endpoint(query string) []byte {
+    resp, err := http.Get(query)
     if err != nil {
         panic(err)
     }
@@ -20,13 +18,36 @@ func main() {
     if err != nil {
         panic(err)
     }
+    return body
+}
+
+
+func main() {
+    type Message struct {
+        Repositories []string `json:"repositories"`
+    }
+    type Message2  struct {
+	Name string   `json:"name"`
+	Tags []string `json:"tags"`
+    }
+    http_json := read_http_endpoint("http://localhost:5000/v2/_catalog")
     var decoded Message
-    err = json.Unmarshal(body, &decoded)
+    err := json.Unmarshal(http_json, &decoded)
     if err != nil {
         panic(err)
     }
     for _,image := range decoded.Repositories {
-        fmt.Println(image)
+        http_tags := strings.Join([]string{"http://localhost:5000/v2/",image,"/tags/list"},"")
+	http_json2 := read_http_endpoint(http_tags)
+        var decoded2 Message2
+	 err := json.Unmarshal(http_json2, &decoded2)
+        if err != nil {
+            panic(err)
+        }
+//        fmt.Println(decoded2.Name)
+	for _,tag := range decoded2.Tags {
+	    fmt.Printf("%s:%s\n",decoded2.Name,tag)
+	}
     }
 
 }
